22a0f17b62aa3315a9a92eff89fd496194897d7a
Ricardo Alvarado <rick96_@hotmail.com>
2021-05-19 15:51:29 -0600

    hola mundo
    se agrego un servicio get que devuelve un hola mundo

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.json('hola mundo');
+})
+
 app.use((req, res, next) => {
     res.status(404).send('Sorry cant find that!');
 });
